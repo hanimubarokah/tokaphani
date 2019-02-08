44e5170a1f775f637d9f01181d654bed799bd061
hanimubarokah <hanimubarokah151@gmail.com>
2019-02-08 14:03:02 +0700

    video-eps4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,8 +55,9 @@
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                         Logout
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
